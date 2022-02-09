ccc308e01a56084c7270d3e32938794ff858926e
Adebayo Aishat <aishatadebayor0@gmail.com>
2022-02-08 19:10:45 -0800

    initial commit

--- app.js
+++ app.js
@@ -4,6 +4,36 @@ $(document).ready(function(){
     
     $('#hi').click(function(){
         $('#project').show(2000);
-        
+        $('#proshow').show(1000)
     });
 });
+
+/*let butt=document.querySelectorAll('.himg-btn')
+let himg=document.querySelectorAll('.himg')
+
+himg.forEach((e) =>{
+  e.addEventListener('click',function(){
+    //  e.style.display='none'
+    butt.forEach((btn)=>{
+        btn.style.display='block';
+    })
+   
+    });
+    e.addEventListener('mouseleave',function(){
+        
+        butt.forEach((btn)=>{
+            btn.style.display='none';
+        })
+       
+        })
+
+   
+});
+
+ e.addEventListener('mouseleave',function(){
+        
+      butt.forEach((btn)=>{
+          btn.style.display='none';
+      })
+     
+      });*/
